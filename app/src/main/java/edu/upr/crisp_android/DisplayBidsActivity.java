d0fe1d82b764384c5a28b30a9efcafb74e7c7eda
Ignacio Fernandez <ifndez@mit.edu>
2016-06-29 10:21:59 -0400

    First commit

---
+++ app/src/main/java/edu/upr/crisp_android/DisplayBidsActivity.java
@@ -0,0 +1,22 @@
+package edu.upr.crisp_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DisplayBidsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_bids);
+    }
+
+    public void editBid(View view) {
+        //TODO
+    }
+
+    public void cancelBid(View view) {
+        //TODO
+    }
+}
